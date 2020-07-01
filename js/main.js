653daed2aa06766056ed4f6980b9fb970a0fde12
Julia <sablina83@yandex.ru>
2020-07-01 20:05:32 +0300

    обработка событий

--- js/main.js
+++ js/main.js
@@ -14,20 +14,20 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// var types = {
-//   palace: {
-//     ru: 'Дворец',
-//   },
-//   flat: {
-//     ru: 'Квартира',
-//   },
-//   house: {
-//     ru: 'Дом',
-//   },
-//   bungalo: {
-//     ru: 'Бунгало ',
-//   }
-// };
+var types = {
+  palace: {
+    ru: 'Дворец',
+  },
+  flat: {
+    ru: 'Квартира',
+  },
+  house: {
+    ru: 'Дом',
+  },
+  bungalo: {
+    ru: 'Бунгало ',
+  }
+};
 
 var times = ['12.00', '13.00', '14.00'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -131,6 +131,71 @@ var deactivatePage = function () {
 deactivatePage();
 
 
+
+var mapFilters = mapActive.querySelector('.map__filters-container');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+var createCards = function (card) {
+  var cardElement = cardTemplate.cloneNode(true);
+  var cardTitle = cardElement.querySelector('.popup__title');
+  var cardAddress = cardElement.querySelector('.popup__text--address');
+  var cardPrice = cardElement.querySelector('.popup__text--price');
+  var cardType = cardElement.querySelector('.popup__type');
+  var cardCapacity = cardElement.querySelector('.popup__text--capacity');
+  var cardTime = cardElement.querySelector('.popup__text--time');
+
+  var cardFeatures = cardElement.querySelector('.popup__features');
+  var cardFeaturesItem = cardFeatures.querySelectorAll('.popup__feature');
+  for (var i = 0; i < cardFeaturesItem.length; i++) {
+    if (card.offer.features.indexOf(cardFeaturesItem[i].classList[1].replace('popup__feature--', '')) < 0) {
+      cardFeaturesItem[i].remove();
+    }
+  }
+
+  var cardDescription = cardElement.querySelector('.popup__description');
+
+  var cardPhotos = cardElement.querySelector('.popup__photos');
+  var cardPhotosImage = cardPhotos.querySelector('img');
+
+  if (card.offer.photos.length > 0) {
+    cardPhotosImage.src = card.offer.photos[0];
+    for (var j = 1; j < card.offer.photos.length; j++) {
+      var cardPhoto = cardPhotosImage.cloneNode(true);
+      cardPhotosImage.src = card.offer.photos[j];
+      cardPhotos.appendChild(cardPhoto);
+    }
+  } else {
+    cardPhotos.remove();
+  }
+
+  var cardAvatar = cardElement.querySelector('.popup__avatar');
+
+  cardTitle.textContent = card.offer.title;
+  cardAddress.textContent = card.offer.address;
+  cardPrice.textContent = card.offer.price + ' ₽/ночь';
+  cardType.textContent = types[card.offer.type].ru;
+  cardCapacity.textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  cardTime.textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  cardDescription.textContent = card.offer.description;
+  cardAvatar.src = card.author.avatar;
+
+  return cardElement;
+};
+
+var renderCards = function () {
+  var fragment = document.createDocumentFragment();
+  // fragment.appendChild(createCards(adverts[0]));
+
+  for (var i = 0; i < adverts.length; i++) {
+    fragment.appendChild(createCards(adverts[i]));
+  }
+
+  mapActive.insertBefore(fragment, mapFilters);
+};
+
+// renderCards();
+
+
 var activatePage = function () {
   mapActive.classList.remove('map--faded');
 
@@ -160,6 +225,28 @@ var activatePage = function () {
   };
 
   renderAdverts();
+
+
+
+  var mapPin = pins.querySelectorAll('.map__pin');
+  mapPin.forEach(function (mappin) {
+    if (!mappin.classList.contains('map__pin--main')) {
+
+      mappin.addEventListener('click', function () {
+        // if (mappin.classList.contains('map__pin--active')) {
+        //   mappin.removeEventListener('click', function () {
+        //     mappin.classList.remove('map__pin--active');
+        //   });
+
+        // }
+        mappin.classList.add('map__pin--active');
+
+
+        renderCards();
+      });
+    }
+  });
+
 
   fieldsetsAdForm.forEach(function (fieldset) {
     fieldset.removeAttribute('disabled');
@@ -241,4 +328,9 @@ mapPinMain.addEventListener('keyup', function (evt) {
 // };
 
 // renderCards();
+
+
+
+
+
 

9f2514e539a73390dc923436346220c6c2eb0aa4
Julia <sablina83@yandex.ru>
2020-07-15 17:38:42 +0300

    Правки

--- js/util.js
+++ js/util.js
@@ -1,15 +1,17 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = window.constants.ESC_KEYCODE;
+  var ENTER_KEYCODE = window.constants.ENTER_KEYCODE;
 
   var isEscEvent = function (evt, action) {
-    if (evt.keyCode === window.constants.ESC_KEYCODE) {
+    if (evt.keyCode === ESC_KEYCODE) {
       action();
     }
   };
 
   var isEnterEvent = function (evt, action) {
-    if (evt.keyCode === window.constants.ENTER_KEYCODE) {
+    if (evt.keyCode === ENTER_KEYCODE) {
       action();
     }
   };
